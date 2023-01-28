4b29b7a3c44469b854b0b09afef4bf0fd9d8e678
Nishant Badhautiya <nishantbadhautiya100@gmail.com>
2023-01-29 00:08:23 +0530

    added practice files

--- java script/Tutorial  js/Code with Harry Latest/practice/xxxxx/index.js
+++ java script/Tutorial  js/Code with Harry Latest/practice/xxxxx/index.js
@@ -1,3 +1,8 @@
-function isSubset() {
-
-}
+console.log(document.cookie);
+document.cookie = "name : nishant"
+document.cookie = "mother : lalita"
+document.cookie = "grandfather : shivdev"
+let key = prompt("Enter the key ");
+let value = prompt("Enter the value ");
+document.cookie = `${encodeURIComponent(key)} = ${encodeURIComponent(value)}`;
+console.log(document.cookie);
