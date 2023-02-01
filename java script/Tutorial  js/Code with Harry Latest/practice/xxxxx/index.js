da3e13206995b72adaaf4e529f083ec8dc7bf404
Nishant Badhautiya <nishantbadhautiya100@gmail.com>
2023-02-01 10:57:58 +0530

    added practice work

--- java script/Tutorial  js/Code with Harry Latest/practice/xxxxx/index.js
+++
@@ -1,8 +0,0 @@
-console.log(document.cookie);
-document.cookie = "name : nishant"
-document.cookie = "mother : lalita"
-document.cookie = "grandfather : shivdev"
-let key = prompt("Enter the key ");
-let value = prompt("Enter the value ");
-document.cookie = `${encodeURIComponent(key)} = ${encodeURIComponent(value)}`;
-console.log(document.cookie);
