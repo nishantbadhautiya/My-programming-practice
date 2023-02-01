5a2340982dae311565ed09daa048148912a63899
Nishant Badhautiya <nishantbadhautiya100@gmail.com>
2023-02-01 22:54:38 +0530

    added practice work

---
+++ java script/Tutorial  js/Code with Harry Latest/practice/video99/index.js
@@ -0,0 +1,3 @@
+const regex = /(Nishant){2}/gi;
+let text = "Nishant is a good boy Nishantnishant  NishantNishant";
+console.log(text.replace(regex, "VERY"));
---
+++ java script/Tutorial  js/Code with Harry Latest/practice/xxxxx/module.js
@@ -0,0 +1,8 @@
+
+const {hello,ahello} = require("./module1");   //destructuring of hello and ahello
+
+hello();
+ahello("Nishant");
+ahello("Aman");
+ahello("Amit");
+ahello("Rohan");
