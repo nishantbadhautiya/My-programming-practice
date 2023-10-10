a500a1977ec166f9bdf34bce5dbd046885c7ed80
Nishant Badhautiya <nishantbadhautiya100@gmail.com>
2023-10-10 10:03:39 +0530

    practice work

--- python/tutorial/code with harry/Projects/Iron Man Jarvis AI Desktop/jarvis.py
+++ python/tutorial/code with harry/Projects/Iron Man Jarvis AI Desktop/jarvis.py
@@ -5,26 +5,52 @@ import speech_recognition as sr
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
 engine.setProperty('voice', voices[0].id)
+
+
 def speak(audio):
     engine.say(audio)
     engine.runAndWait()
+
+
 def wishMe():
     hour = int(datetime.datetime.now().hour)
+    # print(hour)
     if hour >= 0 and hour < 12:
         speak("Good Morning")
     elif hour >= 12 and hour < 18:
         speak("Good Afternoon")
     else:
         speak("Good Evening")
-    speak("I am Jarvis Sir, Please tell me how may I help you")
+    # speak("I am Jarvis Sir, Please tell me how may I help you")
+
+
 def takeCommand():
     '''It take microphone input from the user and returns string output '''
-    r = sr.Recognizer() # Recognizer class help of us to recognize audio
+    r = sr.Recognizer()  # Recognizer class help of us to recognize audio
     with sr.Microphone() as source:
         print("Listening")
-        r.pause_threshold  = 1
+        r.pause_threshold = 0.8
+        r.energy_threshold = 300
         audio = r.listen(source)
-        
+
+    try:
+        print("Recognizing...")
+        query = r.recognize_google(audio, language='en-in')  # converts audio to text 
+        print(f"User said: {query} \n")
+
+    except Exception as e:
+        print(e)
+        print("Say that again please...")
+        return "None"
+    return query
+
 if __name__ == "__main__":
     wishMe()
-    speak("Nishant is a good boy")
+    # speak("Nishant is a good boy")
+    res = takeCommand()
+    print(res)
+
+
+
+
+# 27:30
--- python/tutorial/code with harry/Projects/Iron Man Jarvis AI Desktop/main1.py
+++ python/tutorial/code with harry/Projects/Iron Man Jarvis AI Desktop/main1.py
@@ -2,13 +2,13 @@ import pyttsx3
 
 engine = pyttsx3.init('sapi5') # Initializing the Engine and sapi5 is a microsoft speech api
 voices = engine.getProperty('voices') #Getting Available Voices
-# print(voices)
-# print(voices[0].id) # David is a boy
-# print(voices[1].id) # Zira is a girl
+print(voices)
+print(voices[0].id) # David is a boy
+print(voices[1].id) # Zira is a girl
 engine.setProperty('voice', voices[0].id) #Setting the Voice
 
 def speak(audio):
     engine.say(audio)
     engine.runAndWait()
 if __name__ == "__main__":
-    speak("Nishant is a good boy")
+    speak("I am Nishant")
